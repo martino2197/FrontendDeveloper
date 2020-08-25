1cb90a7c48c0032a1ac4353a4e9402553de8e4d0
martino2197 <martino2197@gmail.com>
2020-08-25 15:16:11 -0500

    Promesas: Al crear una Promesa debemos pasarle por argumento la función que vamos a ejecutar de forma asíncrona, dicha función va a recibir dos parámetros para evaluar si se ejecuto bien la función o si fallo.

--- CursoJQuery-A-Javascript/jquery-to-js/src/js/home.js
+++ CursoJQuery-A-Javascript/jquery-to-js/src/js/home.js
@@ -8,3 +8,59 @@ let cambia = "@LuisMartin"
 function cambiarNombre(nuevoNombre) {
   cambia = nuevoNombre
 }
+
+const getUserAll = new Promise(function(todoBien, todoMal) {
+  // Llamar a un api
+  //Para hacer un delay de tiempo tenemos los timmers
+  //setInterval (una funcion que se ejecuta cada cierto tiempo) y setTimeout(una funcion que se ejecuta una sola vez en determinado tiempo)
+  setTimeout(function() {
+  // luego de 3 
+  
+  // todoBien();
+  todoBien('Se acabo el tiempo 5');
+  }, 5000)
+})
+
+const getUser = new Promise(function(todoBien, todoMal) {
+  // Llamar a un api
+  //Para hacer un delay de tiempo tenemos los timmers
+  //setInterval (una funcion que se ejecuta cada cierto tiempo) y setTimeout(una funcion que se ejecuta una sola vez en determinado tiempo)
+  setTimeout(function() {
+  // luego de 3 
+  
+  // todoBien();
+  todoBien('Se acabo el tiempo 3');
+  }, 3000)
+})
+
+
+
+getUser
+  .then(function() {
+    console.log('Todo esta bien en la vida');
+  }) //then() aplica si todo esta bien en mi promesa
+  .catch(function(message) {
+    console.log(message);
+  }) //catch() aplica cuando nuestra promesa falla
+
+  Promise.all([ /*all() es una funcion que recibe un array de promesas*/
+    getUser,
+    getUserAll,
+  ])
+    .then(function(message) {
+      console.log(message);
+    })
+    .catch(function(message) {
+      console.log(message);
+    })
+
+    Promise.race([ /*race() es una funcion que recibe un array de promesas y devulve el then de la primera promesa en terminar*/
+      getUser,
+      getUserAll,
+    ])
+      .then(function(message) {
+        console.log(message);
+      })
+      .catch(function(message) {
+        console.log(message);
+      })
