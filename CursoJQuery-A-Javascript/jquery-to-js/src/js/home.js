e0e60f9db2016101853d3aeb4b3e6cdd60f24236
martino2197 <martino2197@gmail.com>
2020-08-25 16:33:37 -0500

    Tutorial de Ajax en jQuery y fetch en Javascript: Una característica muy solicitada en cualquier sitio dinámico es solicitar datos a un servidor, denominado API.

--- CursoJQuery-A-Javascript/jquery-to-js/src/js/home.js
+++ CursoJQuery-A-Javascript/jquery-to-js/src/js/home.js
@@ -34,33 +34,61 @@ const getUser = new Promise(function(todoBien, todoMal) {
 })
 
 
-
 getUser
-  .then(function() {
-    console.log('Todo esta bien en la vida');
+  .then(function () {
+    console.log("Todo esta bien en la vida");
   }) //then() aplica si todo esta bien en mi promesa
-  .catch(function(message) {
+  .catch(function (message) {
     console.log(message);
-  }) //catch() aplica cuando nuestra promesa falla
+  }); //catch() aplica cuando nuestra promesa falla
 
-  Promise.all([ /*all() es una funcion que recibe un array de promesas*/
-    getUser,
-    getUserAll,
-  ])
-    .then(function(message) {
-      console.log(message);
-    })
-    .catch(function(message) {
-      console.log(message);
-    })
+Promise.all([
+  /*all() es una funcion que recibe un array de promesas*/ getUser,
+  getUserAll,
+])
+  .then(function (message) {
+    console.log(message);
+  })
+  .catch(function (message) {
+    console.log(message);
+  });
 
-    Promise.race([ /*race() es una funcion que recibe un array de promesas y devulve el then de la primera promesa en terminar*/
-      getUser,
-      getUserAll,
-    ])
-      .then(function(message) {
-        console.log(message);
-      })
-      .catch(function(message) {
-        console.log(message);
-      })
+Promise.race([
+  /*race() es una funcion que recibe un array de promesas y devulve el then de la primera promesa en terminar*/
+  getUser,
+  getUserAll,
+])
+  .then(function (message) {
+    console.log(message);
+  })
+  .catch(function (message) {
+    console.log(message);
+  });
+
+
+// Tutorial de Ajax en jQuery y Javascript
+
+$.ajax('https://randomuser.me/api/', {
+  method: 'GET',
+  success: function(data) {
+    console.log(data);
+  },
+  error: function(error) {
+    console.log(error);
+  }
+})  // esto es un XMLHttpRequest
+
+// Utilizando Javascript vainilla
+
+fetch("https://randomuser.me/api/") //fetch() me va a devolver una promesa
+  .then(function (response) {
+    // console.log(response);
+    return response.json(); //nuestra respuesta tiene un metodo llamado json() y devuelve una PROMESA
+  })
+  .then(function (user) {
+    //este .then() va a reaccionar cuando se acabe de resolver la promesa del response.json(), es una promesa encadenada
+    console.log("user", user.results[0].name.first);
+  })
+  .catch(function() {
+    console.log('algo falló');
+  })
