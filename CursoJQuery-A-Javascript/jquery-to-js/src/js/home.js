0f863cc052f07c1c2e3e335bb8747c91c4582228
martino2197 <martino2197@gmail.com>
2020-08-25 14:13:51 -0500

    Variables y Funciones: const y let es la forma en que se declaran las variables a partir de ECMAScript 6, const sirve para declarar variables que nunca van a ser modificadas y en -

---
+++ CursoJQuery-A-Javascript/jquery-to-js/src/js/home.js
@@ -0,0 +1,10 @@
+console.log('hola mundo!');
+
+// EMACScript 6 const (constante) y let (variable), se depreca var
+const noCambia = "Martin";
+
+let cambia = "@LuisMartin"
+
+function cambiarNombre(nuevoNombre) {
+  cambia = nuevoNombre
+}
