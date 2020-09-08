b55b818825b5ba9424d1c8e5d7c737a984cb72e8
martino2197 <martino2197@gmail.com>
2020-09-07 21:52:37 -0500

    Arrow Functions, Promesas y Parámetros en objetos

--- CursoECMAScript6+/Ecmascript/src/es6/index.js
+++ CursoECMAScript6+/Ecmascript/src/es6/index.js
@@ -86,3 +86,64 @@ console.log(globalVar);
 
 const a = 'b';
 // a = 'a' No se permite la reasignacion
+
+
+
+
+/**Arrow Functions, Promesas y Parámetros en objetos */
+
+/**Parametros en Objetos */
+let name = 'Martin'
+let age = 23
+
+//antes de ecmaScript 6
+obj = { name: name, age: age }
+console.log(obj);
+
+// es6
+obj2 = { name, age }
+console.log(obj2);
+
+
+/**Arrow Funtions */
+
+const names = [
+    {name: 'Martin', age: 23 },
+    {name: 'Luis', age: 24}
+]
+
+//antes de ecmaScript 6
+let listOfNames = names.map(function (item) {
+    console.log(iten.name);
+})
+
+// es6
+let listOfNames2 = names.map(item => console.log(item.name))
+
+const listOfNames3 = (name, age, country) => {
+    ...
+}
+
+//para un solo elemento
+const listOfNames4 = name => {
+    ...
+}
+
+const square = num => num * num; //recibe un numero que sera multiplicado por si mismo y al final lo retorna
+
+/**Promesas */
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if (true) {
+            resolve('Hey!')
+        } else {
+            reject('Upss!!')
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log('hola')) //Puede haber multiples .then()
+    .catch(error => console.log(error))
