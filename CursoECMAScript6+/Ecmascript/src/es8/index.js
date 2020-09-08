31e1db552776f3f942483949b2fd965ecbc8d5c9
martino2197 <martino2197@gmail.com>
2020-09-08 12:20:40 -0500

    ¿Qué se implementó en ES8?: Async Await

--- CursoECMAScript6+/Ecmascript/src/es8/index.js
+++ CursoECMAScript6+/Ecmascript/src/es8/index.js
@@ -1,5 +1,7 @@
 /**Object entries (devuelve los valores de una matriz) */
 
+const { default: hello } = require("../es6/module");
+
 const data = {
     frontend: 'Martin',
     backend: 'Luis',
@@ -32,3 +34,32 @@ const string = 'hello';
 console.log(string.padStart(7, 'hi'));
 console.log(string.padEnd(12, '-----'));
 console.log('food'.padEnd(12, '-----'));
+
+
+/**ASYNC y AWAIT */
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true) 
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    //manejo de errores correctamente
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+anotherFunction();
