51abc64243e5501086000f6c7ab783c7fd69866b
martino2197 <martino2197@gmail.com>
2020-09-09 16:06:22 -0500

    Obtener personajes con la función de llamado a la API

--- CursoSinglePageApplication/cientifico/src/pages/Home.js
+++ CursoSinglePageApplication/cientifico/src/pages/Home.js
@@ -1,15 +1,21 @@
-const Home = () => {
+import getData from '../utils/getData';
+
+const Home = async () => {
+    const characters = await getData();
     const view = `
         <div class="Characters">
-            <articles class="Character-item">
-                <a href="#/1/">
-                    <img src="image" alt="name">
-                    <h2>Name</h2>
-                </a>
-            </articles>
+            ${characters.results.map(character => `
+                <articles class="Character-item">
+                    <a href="#/${character.id}/">
+                        <img src="${character.image}" alt="${character.name}">
+                        <h2>${character.name}</h2>
+                    </a>
+                </articles>
+            `).join('')}  
         </div>
     `;
     return view;
 };
+// USAMOS .join('') para unir cada una de la iteraciones del template y que no use ninguna forma de separarlos
 
 export default Home;
