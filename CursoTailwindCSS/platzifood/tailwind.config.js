03027047659e80d087a3d9d5aec4bfd875e3fa0d
martino2197 <martino2197@gmail.com>
2020-09-12 12:13:23 -0500

    Proyecto PlatziFood: Construye el header, Crea el footer

--- CursoTailwindCSS/platzifood/tailwind.config.js
+++ CursoTailwindCSS/platzifood/tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#f3c614',
+        'secondary': '#353535',
+      }
+    },
   },
   variants: {
     opacity: ['responsive', 'hover', 'focus', 'disabled'],
