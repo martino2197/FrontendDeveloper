71b9f7414ce6fbedc55e29b7066891bcf0dad52d
martino2197 <martino2197@gmail.com>
2020-09-15 01:13:34 -0500

    POSTCSS-PRESET-ENV: CALC, Media Queries, Imagenes retina con Post CSS - image-set, Colores.

--- CursoPostCSS/PostCSS/postcss.config.js
+++ CursoPostCSS/PostCSS/postcss.config.js
@@ -5,13 +5,15 @@ module.exports = {
         // }),
         require('postcss-apply'),
         require("postcss-preset-env") ({
+            stage: 1, //postcss-preset-env permite el uso de custom-media sin embargo, es aceptado en el stage 1 de este plugin. Por defecto, si no especificamos el stage, postcss-preset-env tendrá por defecto el stage 2
             autoprefixer: {
                 grid: true,
                 flexbox: false,
             },
             preserve: true, //Valor por defecto de "true".Se asemeja a customProperties
             //true mantienen root
-	        //cambiar a false para notar el cambio
+            //cambiar a false para notar el cambio
+            calc: false, //Con esta configuración de postcss podemos evitar que la función calc sea transpilada por postcss, ya que es soportada por todos los navegadores.
         })
     ]
 }
