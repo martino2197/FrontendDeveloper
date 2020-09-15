47c958cc71a303af2640bf059c0e84a85817180e
martino2197 <martino2197@gmail.com>
2020-09-14 22:44:55 -0500

    POSTCSS-PRESET-ENV: Intalación, Los nuevos módulos de CSS, Variables.

--- CursoPostCSS/PostCSS/postcss.config.js
+++ CursoPostCSS/PostCSS/postcss.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     plugins: [
-        require('autoprefixer') ({ //De esta manera estoy configurando el autoprefixer
-            grid: true
+        // require('autoprefixer') ({ //De esta manera estoy configurando el autoprefixer
+        //     grid: true
+        // }),
+        require('postcss-apply'),
+        require("postcss-preset-env") ({
+            autoprefixer: {
+                grid: true,
+                flexbox: false,
+            },
+            preserve: true, //Valor por defecto de "true".Se asemeja a customProperties
+            //true mantienen root
+	        //cambiar a false para notar el cambio
         })
     ]
 }
