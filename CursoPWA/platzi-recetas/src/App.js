9d62e72af3f12a5762a167d1e9445d8251a5f75c
martino2197 <martino2197@gmail.com>
2020-09-26 16:31:46 -0500

    Funcionalidades Avanzadas: Trabajando Offline, Notificaciones, Publicando la app.

--- CursoPWA/platzi-recetas/src/App.js
+++ CursoPWA/platzi-recetas/src/App.js
@@ -27,6 +27,9 @@ export default class App extends React.Component {
             <Link to="/">
               Recetas <IfOffline>Offline</IfOffline>
             </Link>
+            <Link to="/timer" className="timerLink">
+              ⏱
+            </Link>
           </header>
 
           <main>
